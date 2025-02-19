57bc797832d85b4aacafae5d9a0fea7d82c61ed5
Dan Kingo <danieldejen23@gmail.com>
2025-02-19 17:25:19 +0300

    add a logger for the requests

--- index.ts
+++ index.ts
@@ -1,11 +1,27 @@
 import express, { Express } from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
 import router from "./routes/jobRouter";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-import { CorsOptions } from "cors";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+const logDirectory = path.join(__dirname, "logs");
+
+if (!fs.existsSync(logDirectory)) {
+}
+const accessLogStream = fs.createWriteStream(
+  path.join(logDirectory, "access.log"),
+  {
+    flags: "a",
+  }
+);
 const whitelist: string[] = [
   "http://localhost:3000",
   "https://www.google.com",
@@ -29,6 +45,7 @@ export default corsOptions;
 
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use("/", router);
 app.listen(port, () => {
   console.log(`server started at port ${port}`);
