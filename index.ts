815f3f3227583ec12886caccfbcbe531428909a5
Dan Kingo <danieldejen23@gmail.com>
2025-02-19 09:49:46 +0300

    add a route to get all jobs

--- index.ts
+++ index.ts
@@ -1,11 +1,62 @@
 import express, { Express, Request, Response } from "express";
 
+interface Jobs {
+  id: string;
+  company: string;
+  salary: number;
+  category:
+    | "Web Development"
+    | "IOS Development"
+    | "Android Development"
+    | "Front-End Development"
+    | "Back-End Development"
+    | "Graphics Design";
+}
+
+const jobs: Jobs[] = [
+  {
+    id: "jobId1",
+    company: "Google",
+    salary: 19_000,
+    category: "Back-End Development",
+  },
+  {
+    id: "jobId1",
+    company: "Google",
+    salary: 19_000,
+    category: "Back-End Development",
+  },
+  {
+    id: "jobId2",
+    company: "Amazon",
+    salary: 14_000,
+    category: "Front-End Development",
+  },
+  {
+    id: "jobId3",
+    company: "Meta",
+    salary: 21_000,
+    category: "IOS Development",
+  },
+  {
+    id: "jobId4",
+    company: "Apple",
+    salary: 21_000,
+    category: "Android Development",
+  },
+  {
+    id: "jobId5",
+    company: "Tiktok",
+    salary: 27_000,
+    category: "Graphics Design",
+  },
+];
 const app: Express = express();
 const port = process.env.PORT || 3000;
 //handle simple routes
 
 app.get("/jobs", (_req: Request, res: Response) => {
-  res.send("<h1>All Jobs here</h1>");
+  res.send(jobs);
 });
 
 app.listen(port, () => {
