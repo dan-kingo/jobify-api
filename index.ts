5b93daa0ff3b4d19501eca3de888e8c535293c84
Dan Kingo <danieldejen23@gmail.com>
2025-02-19 09:36:49 +0300

    setup a simple express server

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import express, { Express, Request, Response } from "express";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const app: Express = express();
+const port = process.env.PORT || 3000;
+//handle simple routes
 
-console.log(__filename);
+app.get("/jobs", (_req: Request, res: Response) => {
+  res.send("<h1>All Jobs here</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
+});
