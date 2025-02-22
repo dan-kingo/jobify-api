5f3a5d7aae5479eac7a8f350d9e3bb2542c45b7e
Dan Kingo <danieldejen23@gmail.com>
2025-02-22 12:25:20 +0300

    handle server and invaild id error

--- controllers/jobControllers.ts
+++ controllers/jobControllers.ts
@@ -1,19 +1,33 @@
 import { Request, Response } from "express";
 import Job from "../models/jobs";
 import { jobSchema } from "../schema/jobSchemas";
+import mongoose from "mongoose";
 
 const getAllJobs = async (_req: Request, res: Response) => {
-  const jobs = await Job.find();
-  res.send(jobs);
+  try {
+    const jobs = await Job.find();
+    res.send(jobs);
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error is occured!" });
+  }
 };
 
 const getJob = async (req: Request<jobSchema>, res: Response) => {
-  const job = await Job.findById(req.params.id);
-  if (!job) {
-    res.status(404).send(`Job with id ${req.params.id} is not found!`);
-    return;
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ message: "Invalid course ID format" });
+      return;
+    }
+    const job = await Job.findById(id);
+    if (!job) {
+      res.status(404).send(`Job with id ${id} is not found!`);
+      return;
+    }
+    res.send(job);
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error is occured!" });
   }
-  res.send(job);
 };
 
 const addJob = async (req: Request<jobSchema>, res: Response) => {
@@ -45,29 +59,47 @@ const addJob = async (req: Request<jobSchema>, res: Response) => {
 };
 
 const updateJob = async (req: Request<jobSchema>, res: Response) => {
-  let updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ message: "Invalid course ID format" });
+      return;
+    }
+    let updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
 
-  if (!updatedJob) {
-    res.status(404).send(`Job with id ${req.params.id} is not found!`);
-    return;
+    if (!updatedJob) {
+      res.status(404).send(`Job with id ${id} is not found!`);
+      return;
+    }
+
+    res.status(200).json({ message: "Job Updated Successfully", updatedJob });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error is occured!" });
   }
-
-  res.status(200).json({ message: "Job Updated Successfully", updatedJob });
 };
 
 const deleteJob = async (req: Request<jobSchema>, res: Response) => {
   //first find the job
-  let result = await Job.findByIdAndDelete(req.params.id);
-  if (!result) {
-    res.status(404).send(`Job with id ${req.params.id} is not found!`);
-    return;
-  }
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ message: "Invalid course ID format" });
+      return;
+    }
+    let result = await Job.findByIdAndDelete(id);
+    if (!result) {
+      res.status(404).send(`Job with id ${id} is not found!`);
+      return;
+    }
 
-  res
-    .status(200)
-    .json({ message: "Job Deleted Successfully", deletedJob: result });
+    res
+      .status(200)
+      .json({ message: "Job Deleted Successfully", deletedJob: result });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error is occured!" });
+  }
 };
 
 export { updateJob, addJob, deleteJob, getAllJobs, getJob };
