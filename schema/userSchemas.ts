897ebdc1f2b52c612b56c502a68d1d781214a95d
Dan Kingo <danieldejen23@gmail.com>
2025-02-22 19:45:10 +0300

    create user model

---
+++ schema/userSchemas.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchemas = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  location: String,
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+});
+
+const User = mongoose.model("User", userSchemas);
+
+export default User;
