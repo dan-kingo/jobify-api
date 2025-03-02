4d8e8eaf43dfee199b5bcde67731b62018703b25
Dan Kingo <danieldejen23@gmail.com>
2025-03-02 10:26:32 +0300

    prevent unauthorized access for the application status

--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -6,11 +6,16 @@ import {
 } from "../controllers/usersControllers";
 import { validateData } from "../middlewares/validationMiddleware";
 import userSchemas from "../schema/userSchemas";
+import authorizeMiddleware from "../middlewares/authorizeMiddleware";
 
 const userRouter = Router();
 
 userRouter.get("/current-user", getCurrentUser);
 userRouter.post("/update-user", validateData(userSchemas), updateUser);
-userRouter.get("/admin/app-stats", getApplicationStats);
+userRouter.get(
+  "/admin/app-stats",
+  authorizeMiddleware(["admin"]),
+  getApplicationStats
+);
 
 export default userRouter;
